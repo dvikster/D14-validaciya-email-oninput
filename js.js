a4f281e12e7d7fc0c7246aa9b71b56a4b9aece6d
dvikster <dvikster@gmail.com>
2017-05-11 16:51:46 +0300

    added files

--- js.js
+++ js.js
@@ -5,11 +5,11 @@ email.oninput = function(){
     var condDog = x.match(/@/g);
     var condDogIndex = x.charAt(0);
     var foundPosDog = x.indexOf(dog);
-	if(condDog  && (condDog.length == 1) && (condDogIndex !== dog) && (foundPosDog+1 !== x.length) ){
+	if(condDog  && (condDog.length == 1) && (condDogIndex !== dog) && (foundPosDog !== x.length-1) ){
         email.className = 'good';
 		for( i=0; i < x.length; i++){
 			if( x[i] == ' ' || x[i] == '"' || x[i] == ')' || x[i] == '(' || x[i] == '/' || x[i] == '\\' || x[i] == '|' || x[i] == ':' || x[i] == ';' || x[i] == '#' || x[i] == '?' || x[i] == ','){
-                email.classList.add('error');
+                email.className = 'error';
 			}
 		}
 	}
