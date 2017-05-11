1a4216cc584f133112f69355ffc7173f5c262118
dvikster <dvikster@gmail.com>
2017-05-11 17:57:52 +0300

    added files

--- js.js
+++ js.js
@@ -4,16 +4,49 @@ email.oninput = function(){
     var dog = '@';
     var condDog = x.match(/@/g);
     var condDogIndex = x.charAt(0);
-    var foundPosDog = x.indexOf(dog);
+    var foundPosDog = x.indexOf('@');
 	if(condDog  && (condDog.length == 1) && (condDogIndex !== dog) && (foundPosDog !== x.length-1) ){
         email.className = 'good';
-		for( i=0; i < x.length; i++){
-			if( x[i] == ' ' || x[i] == '"' || x[i] == ')' || x[i] == '(' || x[i] == '/' || x[i] == '\\' || x[i] == '|' || x[i] == ':' || x[i] == ';' || x[i] == '#' || x[i] == '?' || x[i] == ','){
+        var errors_symbol =[' ','"',')','(', '/','\\','|',':',';','#','?',',',];
+        for (var i=0; i<errors_symbol.length; i++){
+            if (x.indexOf(errors_symbol[i])!=-1) {
                 email.className = 'error';
-			}
-		}
+                return false;
+            }
+        }
+        //или так проверка на плохие символы
+        // for( i=0; i < x.length; i++){
+        //     if( x[i] == ' ' || x[i] == '"' || x[i] == ')' || x[i] == '(' || x[i] == '/' || x[i] == '\\' || x[i] == '|' || x[i] == ':' || x[i] == ';' || x[i] == '#' || x[i] == '?' || x[i] == ','){
+        //         email.className = 'error';
+        //     }
+        // }
+
 	}
     else {
         email.className = 'error';
     }
 }
+
+var tel = document.querySelector('#tel');
+var telNumber='';
+var newtel='';
+
+tel.onkeyup=function() {
+    telNumber = this.value.split('');
+	if (telNumber.length == 3){
+		telNumber.splice(3, 0, '-');
+	}
+    else if (telNumber.length == 6){
+    	telNumber.splice(6, 0, '-');
+	}
+    newtel = telNumber.join('');
+    console.log(newtel);
+    document.querySelector('#tel2').innerHTML = newtel;
+    document.querySelector('#tel').value = newtel;
+}
+
+
+
+// var str = [4,5,4,5,6];
+// str.splice(3, 0, '-');
+// console.log(str);
